de7880ebe72c9d7aba4b80cc349ca09e42663385
alkhlal00 <alkhlaljn@gmail.com>
2025-05-07 18:33:20 +0100

    Update project files

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 export function downloadcv(){
     const link = document.createElement('a');
-    link.href = '/cv.pdf'; 
+    link.href = 'https://drive.google.com/file/d/1LgbPvCo3RR4-Y0tYxlGoK_aFl73PwS6l/view?usp=sharing'; 
     link.download = 'Abderrazaq_Moustaoui.pdf';
     document.body.appendChild(link);
     link.click();
